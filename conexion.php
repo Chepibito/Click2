ef3dab2a34a60ccd55684d2ce9ffa6d83bcff875
Oscar Cabrales <oscar.cbrlc@gmail.com>
2025-12-01 08:10:00 -0600

    Change database host from localhost to IP address

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli("localhost:3306", "brsqfryi_clik012e_CESARDANIEL", "Cesardiosdelawa123", "brsqfryi_clik012e_click");
+$mysqli = new mysqli("185.207.224.10:3306", "brsqfryi_clik012e_CESARDANIEL", "Cesardiosdelawa123", "brsqfryi_clik012e_click");
 
 if ($mysqli->connect_errno) {
 	echo "la conexion a la base de datos no se ha logrado ";
