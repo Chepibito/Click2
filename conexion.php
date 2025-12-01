21407be3c05d8fccedc95366dee29ec8ab6e5e5f
Oscar Cabrales <oscar.cbrlc@gmail.com>
2025-12-01 08:18:31 -0600

    Change database connection details
    Updated database connection credentials in conexion.php.

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli("185.207.224.10:3306", "brsqfryi_clik012e_CESARDANIEL", "Cesardiosdelawa123", "brsqfryi_clik012e_click");
+$mysqli = new mysqli("185.207.224.10:3306", "brsqfryi_admin", "29@X&puz^PJ:FB:", "brsqfryi_click2db");
 
 if ($mysqli->connect_errno) {
 	echo "la conexion a la base de datos no se ha logrado ";
