35514dd35ce4b42aa7c3a2fb966e328f6ceeddca
Oscar Cabrales <oscar.cbrlc@gmail.com>
2025-12-01 07:43:59 -0600

    Fix database name in conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli("localhost:3306", "brsqfryi_clik012e_CESARDANIEL", "Cesardiosdelawa123", "clik012e_click");
+$mysqli = new mysqli("localhost:3306", "brsqfryi_clik012e_CESARDANIEL", "Cesardiosdelawa123", "brsqfryi_clik012e_click");
 
 if ($mysqli->connect_errno) {
 	echo "la conexion a la base de datos no se ha logrado ";
